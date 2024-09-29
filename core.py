ba85845e2d4250259d754e499d175a2574e705ea
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-09-29 15:36:48 -0600

    Update core.py

--- core.py
+++ core.py
@@ -2,6 +2,15 @@ import streamlit as st
 
 class Core:
 
+  def base_gui(self):
+    iterable = ("Diametro", "Conductividad", "Difusividad", "Temperatura inicial", "Temperatura ambiente", "Coeficiente de transferencia",
+                "Densidad", "Calor específico", "Distancia del centro", "Tiempo")
+    datos = []
+
+    for x in iterable:
+      datos.append(st.number_input("iterable"))
+    print(datos)
+      
   
   def __init__(self):
     
@@ -9,6 +18,8 @@ class Core:
 
     st.header("Calculadora")
     st.radio("¿Qué figura vas a analizar?", ("Placa", "Cilindro", "Esfera", "Medio semi-infinito"), horizontal=True)
+    self.base_gui()
+    
     
     
 
