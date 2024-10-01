78d7ff6508c5df40dc7bc7414d14b863400e89fa
Salario-Minimo <164933157+Salario-Minimo@users.noreply.github.com>
2024-09-30 22:14:00 -0600

    Update core.py

--- core.py
+++ core.py
@@ -11,7 +11,8 @@ class Core:
 
     for x in iterable:
       datos.append(float(st.text_input(x, value="0")))
-    st.button("Calcular", on_click=self.selector[self.figura], args=datos)
+    if st.button("Calcular"):
+      self.selector[self.figura](*datos):
       
   
   def __init__(self):
